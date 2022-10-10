8d26e6ddb7bf964b08ab1401a205c0cd6eabd24b
Timofey Kremko <timkremko@gmail.com>
2022-10-10 21:45:22 +0300

    add: fuctionallity to mobile menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import "./header.css"
 
 const Header = () => {
-  const [Toggle, showMenu] = useState(false)
+  const [Toggle, showMenu] = useState(false); 
   return (
     <header className="header">
       <nav className="nav container">
@@ -10,7 +10,7 @@ const Header = () => {
           Smith
         </a>
 
-        <div className="nav__menu">
+        <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
           <ul className="nav__list grid">
             <li className="nav__item">
               <a href="#home" className="nav__link active-link">
@@ -48,9 +48,12 @@ const Header = () => {
               </a>
             </li>
           </ul>
-          <i className="uil uil-times nav__close"></i>
+          <i
+            className="uil uil-times nav__close"
+            onClick={() => showMenu(!Toggle)}
+          ></i>
         </div>
-        <div className="nav__toggle">
+        <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
           <i className="uil uil-apps"></i>
         </div>
       </nav>
