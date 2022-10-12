a5fd9f68ee3faf58a17a511157bb2c960508447c
Timofey Kremko <timkremko@gmail.com>
2022-10-12 20:14:04 +0300

    init: contact section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import About from './components/About/About';
+import Contact from './components/Contact/Contact';
 import Header from './components/Header/Header';
 import Home from './components/home/Home';
 import Qualification from './components/Qualification/Qualification';
@@ -15,6 +16,7 @@ function App() {
         <About /> 
         <Skills />
         <Qualification />
+        <Contact />
       </main>
     </>
   );
