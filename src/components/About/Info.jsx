433b74a606fa44ec5f56ae1b43de3cf4c3196581
Timofey Kremko <timkremko@gmail.com>
2022-10-14 12:54:25 +0200

    fix: about info

--- src/components/About/Info.jsx
+++ src/components/About/Info.jsx
@@ -16,7 +16,7 @@ const Info = () => {
       <div className="about__box">
         <i className="bx bx-support about__icon"></i>
         <h3 className="about__title">Support</h3>
-        <span className="about__subtitle">Online 34/7</span>
+        <span className="about__subtitle">Online 24/7</span>
       </div>
     </div>
   );
